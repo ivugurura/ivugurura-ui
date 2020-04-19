556f95231792028e3e1b2296554ebbf2dc72348e
Akimana Linux <akimana@linux.me>
2020-04-19 17:08:15 +0200

    Design the landing page

---
+++ src/utils/styles.js
@@ -0,0 +1,18 @@
+const colors = {
+  primary: '#022147',
+  accent: '#a7c9e3',
+  secondary: '#fff',
+};
+export const bgStyles = {
+  bgPrimary: {
+    backgroundColor: colors.primary,
+  },
+  bgAccent: {
+    backgroundColor: colors.accent,
+  },
+};
+export const textStyles = {
+  textTransparent: {
+    color: colors.secondary,
+  },
+};

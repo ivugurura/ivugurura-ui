4350ca0ecc7fa31919ac8e92e78d7be18e3060ce
Akimana Linux <akimana@linux.me>
2020-06-03 19:06:57 +0200

    Get the latest topic

--- src/components/common/RecentTopics.js
+++ src/components/common/RecentTopics.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Media, Card } from 'react-bootstrap';
 import { textStyles } from '../../utils/styles';
 import { translate } from '../utils';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTopics } from '../../redux/actions';
+import { Loading } from './Loading';
+import { formatDate } from '../../utils/constants';
 
 export const RecentTopics = () => {
+  const dispatch = useDispatch();
+  const { recentLoading, recentTopics } = useSelector(({ topic }) => topic);
+  useEffect(() => {
+    dispatch(getTopics({ page: 1, pageSize: 3, category: 'recent' }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getTopics]);
   return (
     <Card.Body style={textStyles.textTransparent}>
       <Card.Title style={textStyles.textFtTitle}>
         {translate('recentTopics')}
       </Card.Title>
-      {[1, 2, 3].map((el) => (
-        <Media.Body key={el}>
-          <h6>{`Icyigisho cya ${el}`}</h6>
-          <p>April, 20</p>
-        </Media.Body>
-      ))}
+      {recentLoading ? (
+        <Loading />
+      ) : recentTopics.length ? (
+        recentTopics.map((topic, topicIndex) => (
+          <Media.Body key={topicIndex}>
+            <h6>{topic.title}</h6>
+            <p>{formatDate(topic.createdAt)}</p>
+          </Media.Body>
+        ))
+      ) : (
+        <h4>No topics</h4>
+      )}
     </Card.Body>
   );
 };

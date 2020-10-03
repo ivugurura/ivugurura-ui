4d1ce970a80cb3d7f82b7410ad3e99021c7cb3f7
Akimana Jean d Amour <akimanaja17@gmail.com>
2020-10-03 07:31:20 +0200

    Set to socket url

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -8,7 +8,7 @@ if (localStorage.user) {
 const lang = localStorage.lang || 'kn';
 
 export const http = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL}/socket.io/`,
   withCredentials: true,
   headers: {
     Authorization: token,

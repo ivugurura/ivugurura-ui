7aa4e5e8bc1981202bb006164b56d41d488a0250
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-01-15 09:58:35 +0200

    Design the dowmload messages

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import H5AudioPlayer from 'react-h5-audio-player';
-import { Card } from 'react-bootstrap';
+import H5AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
+import { ButtonGroup, Card, Button } from 'react-bootstrap';
 import { ListGroup } from 'react-bootstrap';
 import { ListGroupItem } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { getMedias } from '../redux/actions';
 import { Loading } from './common';
 import { audioPath } from '../helpers/utils';
-import { Button } from 'react-bootstrap';
 
 export const AudioPlayer = () => {
 	const [currentAudio, setCurrentAudio] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(-1);
 	const { medias, mediasFetching } = useSelector(({ media }) => media);
 	useEffect(() => {
 		getMedias('audio');
@@ -20,9 +20,40 @@ export const AudioPlayer = () => {
 			setCurrentAudio(medias[0]);
 		}
 	}, [medias]);
+	useEffect(() => {
+		if (currentAudio.id) {
+			const theIndex = medias.findIndex((m) => m.id === currentAudio.id);
+			setCurrentIndex(theIndex);
+		}
+	}, [currentAudio, medias]);
+	const customControls = [
+		RHAP_UI.LOOP,
+		<Button
+			size='sm'
+			disabled={currentIndex === 0}
+			onClick={() => setCurrentAudio(medias[currentIndex - 1])}
+		>
+			<i className='fa fa-step-backward'></i>
+		</Button>,
+		<Button
+			size='sm'
+			disabled={currentIndex === medias.length - 1}
+			onClick={() => setCurrentAudio(medias[currentIndex + 1])}
+		>
+			<i className='fa fa-step-forward'></i>
+		</Button>
+	];
+	const BASE_DOWNLOAD = process.env.REACT_APP_API_URL.includes('localhost')
+		? `${process.env.REACT_APP_API_URL}`
+		: '';
+	const DL_ROUTE = BASE_DOWNLOAD + '/api/v1/albums/download/';
 	return (
 		<Card>
-			<H5AudioPlayer autoPlay src={audioPath + currentAudio.mediaLink} />
+			<H5AudioPlayer
+				autoPlay={false}
+				src={audioPath + currentAudio.mediaLink}
+				customAdditionalControls={customControls}
+			/>
 			<Card.Body>
 				<Card.Text>{currentAudio.title}</Card.Text>
 			</Card.Body>
@@ -39,13 +70,23 @@ export const AudioPlayer = () => {
 								key={mediaIndex}
 							>
 								{media.title}
-								<Button
+								<ButtonGroup
 									size='sm'
-									onClick={() => setCurrentAudio(media)}
+									aria-label='Actions buttons'
 									className='pull-right'
 								>
-									Play
-								</Button>
+									<Button size='sm' onClick={() => setCurrentAudio(media)}>
+										<i className='fa fa-play'></i>
+									</Button>
+									<a
+										className='btn'
+										rel='noreferrer'
+										href={DL_ROUTE + media.id}
+										target='_blank'
+									>
+										<i className='fa fa-download'></i>
+									</a>
+								</ButtonGroup>
 							</ListGroupItem>
 						))}
 					</div>

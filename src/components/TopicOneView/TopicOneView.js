5d1edf1e25eaf086fb505361af3c209cf13b847b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-01-22 08:12:53 +0200

    Disable frontend display topic

--- src/components/TopicOneView/TopicOneView.js
+++ src/components/TopicOneView/TopicOneView.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import HtmlParser from 'react-html-parser';
-import { useTranslation } from 'react-i18next';
-import { Comments } from './Comments';
-import { CommentaryForm } from './CommentaryForm';
-import { formatDate, IMAGE_PATH } from 'utils/constants';
+import React from "react";
+import { Card } from "react-bootstrap";
+// import HtmlParser from 'react-html-parser';
+import { useTranslation } from "react-i18next";
+import { Comments } from "./Comments";
+import { CommentaryForm } from "./CommentaryForm";
+import { formatDate, IMAGE_PATH } from "utils/constants";
 
 export const TopicOneView = ({
-	topic = {},
-	topicSlug = '',
-	showComments = false,
-	topicRef = null
+  topic = {},
+  topicSlug = "",
+  showComments = false,
+  topicRef = null,
 }) => {
-	const { t } = useTranslation();
-	return (
-		<>
-			<Card.Title>{topic.title}</Card.Title>
-			<Card.Subtitle className='mb-2 text-muted'>
-				<h6>
-					{t('app:createdAt')} {`${formatDate(topic.createdAt)}`}
-				</h6>
-			</Card.Subtitle>
-			<Card ref={topicRef}>
-				<Card.Img
-					variant='top'
-					src={`${IMAGE_PATH}/${topic.coverImage}`}
-					alt={topic.description}
-				/>
-				<Card.Body>
-					{HtmlParser(topic.content)}
-					<strong>{t('app:title')}</strong>
-					{showComments && <Comments slug={topicSlug} />}
-				</Card.Body>
-				<Card.Footer>
-					{showComments && <CommentaryForm slug={topicSlug} />}
-				</Card.Footer>
-			</Card>
-		</>
-	);
+  const { t } = useTranslation();
+  return (
+    <>
+      <Card.Title>{topic.title}</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">
+        <h6>
+          {t("app:createdAt")} {`${formatDate(topic.createdAt)}`}
+        </h6>
+      </Card.Subtitle>
+      <Card ref={topicRef}>
+        <Card.Img
+          variant="top"
+          src={`${IMAGE_PATH}/${topic.coverImage}`}
+          alt={topic.description}
+        />
+        <Card.Body>
+          {/* {HtmlParser(topic.content)} */}
+          <strong>{t("app:title")}</strong>
+          {showComments && <Comments slug={topicSlug} />}
+        </Card.Body>
+        <Card.Footer>
+          {showComments && <CommentaryForm slug={topicSlug} />}
+        </Card.Footer>
+      </Card>
+    </>
+  );
 };

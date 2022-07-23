6e9b11a21e68660e617b3ee7059cfbfbfbe115c2
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-07-23 12:51:14 +0200

    Create nav

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import jwtDecode from 'jwt-decode';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
-import { ToastContainer } from 'react-toastify';
-import { store } from './redux/store';
-import { routes } from './routes';
-import { setUser } from './redux/actions';
-import './i18n';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { Provider } from "react-redux";
+import jwtDecode from "jwt-decode";
+import { BrowserRouter as Router } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+import { ToastContainer } from "react-toastify";
+import { store } from "./redux/store";
+import { routes } from "./routes";
+import { setUser } from "./redux/actions";
+import "./i18n";
+import { useTranslation } from "react-i18next";
+import { Navbar } from "./components/Navbar";
 
 if (localStorage.user) {
-	const user = JSON.parse(localStorage.user);
-	if (user.token) {
-		const tokenInfo = jwtDecode(user.token);
-		const currentTime = Date.now() / 1000;
-		store.dispatch(setUser(user));
+  const user = JSON.parse(localStorage.user);
+  if (user.token) {
+    const tokenInfo = jwtDecode(user.token);
+    const currentTime = Date.now() / 1000;
+    store.dispatch(setUser(user));
 
-		if (tokenInfo.exp < currentTime) {
-			localStorage.removeItem('user');
-			window.location.href = '/';
-		}
-	}
+    if (tokenInfo.exp < currentTime) {
+      localStorage.removeItem("user");
+      window.location.href = "/";
+    }
+  }
 }
 export const App = () => {
-	const { t } = useTranslation(['app']);
-	return (
-		<Provider store={store}>
-			<ToastContainer />
-			<Router>{renderRoutes(routes, { t })}</Router>
-		</Provider>
-	);
+  const { t } = useTranslation(["app"]);
+  return (
+    <Provider store={store}>
+      <Navbar />
+      {/* <ToastContainer />
+      <Router>{renderRoutes(routes, { t })}</Router> */}
+    </Provider>
+  );
 };

bd22ef92bb0411e027ba1851046db6e8fb89733f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-10-14 23:02:44 +0200

    Add alphabetization

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import React, { Suspense } from 'react';
 // import { store } from "./redux/store";
 // import "./i18n";
 import { CssBaseline, LinearProgress, ThemeProvider } from '@mui/material';
-import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import { AppRoutes } from './routes';
 import theme from './common/theme';
+import { AppRoutes } from './routes';
 // import { Route } from "react-router";
 
 const App = () => {

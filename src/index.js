6f03f75a711ee26fa4d55c9efbf25223d9fb9eef
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-11-12 06:42:06 +0200

    Add audio UI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
+import 'react-h5-audio-player/lib/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 import { Card } from '@material-ui/core';
+import { MusicNote, PlayArrow } from '@mui/icons-material';
 import {
   CardContent,
   CardHeader,
+  Divider,
   Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
 } from '@mui/material';
+import AudioPlayer from 'react-h5-audio-player';
 
 import { Page } from '../../common/components/wrappers';
 // import { useHomeStyles } from '../styles';
@@ -42,10 +51,67 @@ export const Home = () => {
         <CardHeader title="Recent topics" subheader="Recents" />
         <CardContent>
           <Grid container spacing={1}>
-            <Grid item xs={12} sm={12} md={3}><TopicItem /></Grid>
-            <Grid item xs={12} sm={12} md={3}><TopicItem /></Grid>
-            <Grid item xs={12} sm={12} md={3}><TopicItem /></Grid>
-            <Grid item xs={12} sm={12} md={3}><TopicItem /></Grid>
+            {[1, 2, 3, 4].map((item) => (
+              <Grid key={item} item xs={12} sm={12} md={3}><TopicItem /></Grid>
+            ))}
+          </Grid>
+        </CardContent>
+      </Card>
+      <Card color="red">
+        <CardHeader title="Audio and video" subheader="Recents" />
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid item xs={12} sm={12} md={5}>
+              <Card>
+                <AudioPlayer
+                  autoPlay
+                  src="http://example.com/audio.mp3"
+                  onPlay={() => console.log('onPlay')}
+                />
+                <CardHeader
+                  title="to Scott, Alex, Jennifer"
+                  subheader={
+                  (
+                    <>
+                      <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        by Ivugurura n ubugorozi
+                      </Typography>
+                      {" — Wish I could come, but I'm out of town this…"}
+                    </>
+)
+                }
+                />
+                <CardContent>
+                  <List>
+                    {[1, 2, 3].map((item) => (
+                      <>
+                        <ListItem
+                          key={item}
+                          secondaryAction={(
+                            <IconButton edge="end">
+                              <PlayArrow />
+                            </IconButton>
+                          )}
+                          alignItems="flex-start"
+                        >
+                          <ListItemIcon>
+                            <MusicNote />
+                          </ListItemIcon>
+                          <ListItemText primary="Song 1" secondary="Some description" />
+                        </ListItem>
+                        <Divider variant="insert" component="li" />
+                      </>
+                    ))}
+                  </List>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={12} md={7}><TopicItem /></Grid>
           </Grid>
         </CardContent>
       </Card>

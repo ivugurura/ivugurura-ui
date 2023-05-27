21f2f21dd478311e0b55c88d670104c56c04e0f2
Jean dAmour AKIMANA(MBP) <akimanaja17@gmail.com>
2023-05-27 20:01:45 +0200

    Add audio player

--- src/pages/admin/MediaEditor.js
+++ src/pages/admin/MediaEditor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Grid } from '@mui/material';
+import { Card, CardHeader, Grid, Typography } from '@mui/material';
+import AudioPlayer from 'react-h5-audio-player';
 
 import { RRVTable } from '../../common/components/RRVTable/Table';
 
@@ -12,8 +13,32 @@ export const MediaEditor = () => {
   return (
     <DashboardContainer title="All media">
       <Grid container spacing={1}>
-        <Grid xs={12} lg={8}>
+        <Grid item xs={12} lg={8}>
           <RRVTable columns={audioColumns()} data={[]} />
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Card>
+            <AudioPlayer
+              autoPlay
+              src="http://example.com/audio.mp3"
+              onPlay={() => console.log('onPlay')}
+            />
+            <CardHeader
+              title="to Scott, Alex, Jennifer"
+              subheader={
+                <>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary">
+                    by Ivugurura n ubugorozi
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </>
+              }
+            />
+          </Card>
         </Grid>
       </Grid>
     </DashboardContainer>

90b2d2c50f925c1cfcceefa2e89dde741e801cd1
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-06-15 20:08:58 +0200

    Set up global error handler

--- src/redux/errorHandler.js
+++ src/redux/errorHandler.js
@@ -1,31 +1,31 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit';
-import isPromise from 'is-promise';
+// import isPromise from 'is-promise';
 import { toast } from 'react-toastify';
 
-export const errorHandler = () => (next) => (action) => {
-  if (!isPromise(action.payload)) {
-    return next(action);
-  }
+// export const errorHandler = () => (next) => (action) => {
+//   if (!isPromise(action.payload)) {
+//     return next(action);
+//   }
 
-  if (!action.meta || !action.meta.localError) {
-    return next(action).catch((error) => {
-      let errorMessage = '';
-      if (error.response) {
-        const { error: message } = error.response.data;
-        errorMessage = message;
-      } else {
-        errorMessage = error.message;
-      }
-      toast(errorMessage, {
-        type: toast.TYPE.ERROR,
-        position: toast.POSITION.BOTTOM_RIGHT,
-        toastId: 13,
-      });
-    });
-  }
+//   if (!action.meta || !action.meta.localError) {
+//     return next(action).catch((error) => {
+//       let errorMessage = '';
+//       if (error.response) {
+//         const { error: message } = error.response.data;
+//         errorMessage = message;
+//       } else {
+//         errorMessage = error.message;
+//       }
+//       toast(errorMessage, {
+//         type: toast.TYPE.ERROR,
+//         position: toast.POSITION.BOTTOM_RIGHT,
+//         toastId: 13,
+//       });
+//     });
+//   }
 
-  return next(action);
-};
+//   return next(action);
+// };
 // import { toast } from 'your-cool-library'
 
 /**
@@ -35,24 +35,15 @@ export const rtkQueryErrorLogger = () => (next) => (action) => {
   // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood,
   // so we're able to utilize these matchers!
   if (isRejectedWithValue(action)) {
-    let errorMessage = 'Rejected';
+    let errorMessage = action.payload?.error;
     if (action.payload?.data) {
-      errorMessage = action.payload?.data.message;
+      errorMessage = action.payload?.data.error;
     }
-    console.log('We got a rejected action!');
-    console.log(action);
     toast(errorMessage, {
       type: toast.TYPE.ERROR,
       position: toast.POSITION.BOTTOM_RIGHT,
       toastId: 13,
     });
-    // toast.error({
-    //   title: 'Async error!',
-    //   message: errorMessage,
-    //   type: toast.TYPE.ERROR,
-    //   position: toast.POSITION.BOTTOM_RIGHT,
-    //   toastId: 13,
-    // });
   }
 
   return next(action);

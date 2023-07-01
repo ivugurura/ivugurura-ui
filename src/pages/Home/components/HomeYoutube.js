b9fa9e34da87dac4056fa6a1c313110f6dcb468c
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-07-01 12:07:21 +0200

    Youtube component for all site

--- src/pages/Home/components/HomeYoutube.js
+++ src/pages/Home/components/HomeYoutube.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import YouTube from 'react-youtube';
 
 import { useWindowSize } from '../../../common/hooks/useWindowSize';
-import { actions } from '../../../redux/actions';
+// import { actions } from '../../../redux/actions';
 
 import { YoutubeLoader } from './HomeLoaders';
 
-export const HomeYoutube = () => {
+export const HomeYoutube = ({
+  widthRatio = 0.5, heightRatio = 0.47, normalHeight = 370, randomize = false,
+}) => {
+  const [videoId, setVideoId] = React.useState('-PweyZWNcLk');
+  const [playerSizes, setPlayerSizes] = React.useState({});
   const { width: winWidth, height: winHeight } = useWindowSize();
-  const { data: ytData, isFetching } = actions.useListYoutubesQuery();
-  const getOptions = () => {
-    let height = 0.47 * winHeight;
-    let width = 0.5 * winWidth;
+  // const { data: ytData, isFetching } = actions.useListYoutubesQuery();
+  const { data: ytData, isFetching } = { data: { items: [], isFetching: false } };
+  useEffect(() => {
+    let height = heightRatio * winHeight;
+    let width = widthRatio * winWidth;
     if (winWidth < 900) {
       width = winWidth;
     }
     if (winHeight < 789) {
-      height = 370;
+      height = normalHeight;
     }
-    return {
-      height,
-      width,
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
-    };
-  };
-  const videoId = ytData?.items?.length > 0 ? ytData?.items?.[0]?.id.videoId : '-PweyZWNcLk';
+    setPlayerSizes({ height, width });
+  }, [widthRatio, heightRatio, normalHeight]);
+
+  useEffect(() => {
+    if (ytData?.items.length > 0) {
+      if (randomize) {
+        const randomIndex = Math.floor(Math.random() * ytData.items.length);
+        setVideoId(ytData.items[randomIndex].id.videoId);
+      } else {
+        setVideoId(ytData.items[0].id.videoId);
+      }
+    }
+  }, [ytData, randomize]);
 
   return isFetching ? <YoutubeLoader />
-    : <YouTube videoId={videoId} opts={getOptions()} />;
+    : (
+      <YouTube
+        videoId={videoId}
+        opts={{
+          ...playerSizes,
+          playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+          },
+        }}
+      />
+    );
 };

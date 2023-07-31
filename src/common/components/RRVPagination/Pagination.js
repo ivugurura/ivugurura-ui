79c17eae8aa3ee6f36808b618e58612e5c80d950
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-07-31 11:12:27 +0200

    Add pagination

--- src/common/components/RRVPagination/Pagination.js
+++ src/common/components/RRVPagination/Pagination.js
@@ -2,16 +2,11 @@ import * as React from 'react';
 
 import { TablePagination } from '@mui/material';
 
+import { usePagination } from '../../hooks/usePagination';
+
 export const RRVPagination = ({ page = 1, pageSize = 10, dataCount = 0 }) => {
-  const [pagination, setPagination] = React.useState({ pageSize, page });
+  const { pagination, handleChangePage, handleChangeRowsPerPage } = usePagination(page, pageSize);
 
-  const handleChangePage = (_, newPage) => {
-    setPagination((prev) => ({ ...prev, page: newPage }));
-  };
-
-  const handleChangeRowsPerPage = ({ target }) => {
-    setPagination((prev) => ({ ...prev, pageSize: parseInt(target.value, 10), page: 1 }));
-  };
   if (dataCount === 0) return null;
   return (
     <TablePagination
---
+++ src/common/hooks/usePagination.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const usePagination = (page = 1, pageSize = 10) => {
+  const [pagination, setPagination] = React.useState({ pageSize, page });
+
+  const handleChangePage = (_, newPage) => {
+    setPagination((prev) => ({ ...prev, page: newPage }));
+  };
+
+  const handleChangeRowsPerPage = ({ target }) => {
+    setPagination((prev) => ({ ...prev, pageSize: parseInt(target.value, 10), page: 1 }));
+  };
+  return { pagination, handleChangePage, handleChangeRowsPerPage };
+};

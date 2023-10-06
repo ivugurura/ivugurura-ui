f4c106da79c154ca0580f11415fa7dd636ab22fb
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-10-06 10:15:48 +0200

    Add fix

--- src/common/components/RRVTable/Table.js
+++ src/common/components/RRVTable/Table.js
@@ -2,4 +2,6 @@ import React from 'react';
 
 import MaterialReactTable from 'material-react-table';
 
-export const RRVTable = ({ ...rest }) => <MaterialReactTable positionActionsColumn="last" {...rest} />;
+export const RRVTable = ({ ...rest }) => (
+  <MaterialReactTable positionActionsColumn="last" {...rest} />
+);

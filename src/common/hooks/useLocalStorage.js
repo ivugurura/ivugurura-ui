2058b8dfd86d3b10baa8aef4c07a1e82b5bdd85e
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-11-20 18:22:01 +0200

    Use local storage hook

---
+++ src/common/hooks/useLocalStorage.js
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useLocalStorage = (key='',value='') => {
+  const rawValueRef = useRef(null);
+
+  const [value, setValue] = useState(() => {
+    if (typeof window === 'undefined') return value;
+
+    try {
+      rawValueRef.current = window.localStorage.getItem(key);
+      const res = rawValueRef.current || value;
+      return res;
+    } catch (e) {
+      console.log(e);
+      return value;
+    }
+  });
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const updateLocalStorage = () => {
+      // Browser ONLY dispatch storage events to other tabs, NOT current tab.
+      // We need to manually dispatch storage event for current tab
+      if (value !== undefined) {
+        const newValue = JSON.stringify(value);
+        const oldValue = rawValueRef.current;
+        rawValueRef.current = newValue;
+        window.localStorage.setItem(key, newValue);
+        window.dispatchEvent(
+          new StorageEvent('storage', {
+            storageArea: window.localStorage,
+            url: window.location.href,
+            key,
+            newValue,
+            oldValue
+          })
+        );
+      } else {
+        window.localStorage.removeItem(key);
+        window.dispatchEvent(
+          new StorageEvent('storage', {
+            storageArea: window.localStorage,
+            url: window.location.href,
+            key
+          })
+        );
+      }
+    };
+
+    try {
+      updateLocalStorage();
+    } catch (e) {
+      console.log(e);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    const handleStorageChange = (e) => {
+      if (e.key !== key || e.storageArea !== window.localStorage) return;
+
+      try {
+        if (e.newValue !== rawValueRef.current) {
+          rawValueRef.current = e.newValue;
+          setValue(e.newValue ? JSON.parse(e.newValue) : undefined);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    if (typeof window === 'undefined') return;
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, [key]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;

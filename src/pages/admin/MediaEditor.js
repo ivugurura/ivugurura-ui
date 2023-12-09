806c98d50fa0a93656bf101bbdd73207b23617e3
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-09 15:56:19 +0200

    Get list of audios

--- src/pages/admin/MediaEditor.js
+++ src/pages/admin/MediaEditor.js
@@ -1,52 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Card, CardContent, CardHeader, Grid, Typography,
 } from '@mui/material';
+import moment from 'moment';
 import AudioPlayer from 'react-h5-audio-player';
 
 import { RRVTable } from '../../common/components/RRVTable/Table';
+import { toAssetPath } from '../../helpers/utils/constants';
+import { actions, initials } from '../../redux/apiSliceBuilder';
 
 import { audioColumns } from './columns/audioColumns';
 import { DashboardContainer } from './components/DashboardContainer';
 
-const audios = [{
-  title: 'Ikirema', type: 'Audio', author: 'Muhagururke tugorore', downloads: 2, shares: 34,
-}];
 export const MediaEditor = () => {
-  console.log('MediaEditor');
+  const [currentAudio, setCurrentAudio] = useState({ index: -1, audio: null });
+  const { data, isFetching } = actions.useListAudiosQuery({ page: 1, pageSize: 3 });
+  const { data: audios } = data || initials.dataArr;
+  useEffect(() => {
+    if (audios?.length > 0) {
+      setCurrentAudio({ index: 0, audio: audios[0] });
+    }
+  }, [audios]);
+  const { audio } = currentAudio;
   return (
     <DashboardContainer title="All media">
       <Grid container spacing={1}>
         <Grid item xs={12} lg={8}>
-          <RRVTable columns={audioColumns()} data={audios} />
+          <RRVTable columns={audioColumns()} data={audios} isLoading={isFetching} />
         </Grid>
         <Grid item xs={12} lg={4}>
-          <Card>
-            <AudioPlayer
-              autoPlay
-              src="http://example.com/audio.mp3"
-              onPlay={() => console.log('onPlay')}
-            />
-            <CardHeader
-              title="to Scott, Alex, Jennifer"
-              subheader={(
-                <>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    by Ivugurura n ubugorozi
-                  </Typography>
-                  {" — Wish I could come, but I'm out of town this…"}
-                </>
+          {audio && (
+            <Card>
+              <AudioPlayer
+                autoPlay
+                src={toAssetPath(audio.mediaLink, false)}
+                onPlay={() => console.log('onPlay')}
+              />
+              <CardHeader
+                title={audio.title}
+                subheader={(
+                  <>
+                    <Typography
+                      sx={{ display: 'inline' }}
+                      component="span"
+                      variant="body2"
+                      color="text.primary"
+                    >
+                      by
+                      {' '}
+                      {audio.author}
+                    </Typography>
+                    {` — Language: ${audio.language?.name}, Album: ${audio.album?.name}`}
+                  </>
               )}
-            />
-            <CardContent>Kinyarwanda</CardContent>
-            <CardContent>Saturday, Dec 25, 2023</CardContent>
-          </Card>
+              />
+              <CardContent>{audio.language?.name}</CardContent>
+              <CardContent>{moment(audio.actionDate).format('dddd, MMM D, YYYY')}</CardContent>
+            </Card>
+          )}
         </Grid>
       </Grid>
     </DashboardContainer>

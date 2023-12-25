2799eb112d521a82b0527f6fa4845dc2e6bac3cc
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-25 22:50:50 +0200

    Apply the form

--- src/pages/admin/MediaEditor/AddEditMedia.js
+++ src/pages/admin/MediaEditor/AddEditMedia.js
@@ -7,12 +7,19 @@ import {
   DialogContent,
   DialogContentText,
   DialogActions,
-  FormControl, Grid, InputLabel, Input, FormHelperText,
 } from '@mui/material';
 
-// const initialStates = {
-//   title: '', type: '', albumId: '', author: '', actionDate: '', mediaLink: '',
-// };
+import { RRVForm } from '../../../common/components/RRVForm';
+
+const fields = [
+  [
+    {
+      label: 'Media title',
+      name: 'title',
+      helperText: 'Name of the media',
+    },
+  ],
+];
 export const AddEditMedia = ({ open, onClose }) => {
   // const [media, setMedia] = React.useState(initialStates);
   console.log('AddEditMedia');
@@ -23,15 +30,7 @@ export const AddEditMedia = ({ open, onClose }) => {
         <DialogContentText>
           Media: a file that can be (songs, preachings). Audio, video, images.
         </DialogContentText>
-        <Grid container spacing={2}>
-          <Grid item lg={6} md={6} sm={12} xs={12}>
-            <FormControl variant="standard" fullWidth>
-              <InputLabel htmlFor="my-input">Email address</InputLabel>
-              <Input id="my-input" aria-describedby="my-helper-text" />
-              <FormHelperText id="my-helper-text">We will never share your email.</FormHelperText>
-            </FormControl>
-          </Grid>
-        </Grid>
+        <RRVForm fields={fields} />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>

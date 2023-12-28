a93ac8c0e56e4dbb6438b364d5d05eb8e062725f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-28 19:31:18 +0200

    Add media fields

--- src/pages/admin/MediaEditor/AddEditMedia.js
+++ src/pages/admin/MediaEditor/AddEditMedia.js
@@ -8,39 +8,21 @@ import {
   DialogContentText,
   DialogActions,
 } from '@mui/material';
+import moment from 'moment';
 
 import { RRVForm } from '../../../common/components/RRVForm';
+import { mediaSchema } from '../../../helpers/formSchemas/media';
 
-const fields = (albums = []) => [
-  [
-    {
-      label: 'Media title',
-      name: 'title',
-      helperText: 'Name of the media',
-    },
-  ],
-  [
-    {
-      label: 'Media type',
-      name: 'type',
-      select: true,
-      options: [{ value: 'audio', label: 'Audio' }],
-      helperText: 'Type of the media',
-    },
-    {
-      label: 'Media album',
-      name: 'albumId',
-      select: true,
-      options: albums,
-      valueSelector: 'id',
-      labelSelector: 'name',
-      helperText: 'Select media album',
-    },
-  ],
-];
+const initialStates = {
+  title: '',
+  type: 'audio',
+  albumId: '',
+  author: '',
+  actionDate: moment().format('YYYY-MM-DD'),
+};
 export const AddEditMedia = ({ open, onClose, albums }) => {
-  // const [media, setMedia] = React.useState(initialStates);
-  console.log('AddEditMedia');
+  const [media, setMedia] = React.useState(initialStates);
+  console.log('AddEditMedia', media);
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Create a new media</DialogTitle>
@@ -48,7 +30,7 @@ export const AddEditMedia = ({ open, onClose, albums }) => {
         <DialogContentText>
           Media: a file that can be (songs, preachings). Audio, video, images.
         </DialogContentText>
-        <RRVForm fields={fields(albums)} />
+        <RRVForm fields={mediaSchema(albums)} states={media} setStates={setMedia} />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>

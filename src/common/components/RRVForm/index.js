6b917e20b04a2007be027d4698ad040f3e3e1f3c
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-28 19:30:43 +0200

    Refactor and fix form

--- src/common/components/RRVForm/index.js
+++ src/common/components/RRVForm/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Grid } from '@mui/material';
 
+import { RRVFileUpload } from '../RRVFileUpload';
+
 import { RRVInput } from './RRVInput';
 
-export const RRVForm = ({ fields = [] }) => {
-  const getFieldView = ({ fieldType, ...viewProps }, idx) => {
+export const RRVForm = ({ fields = [], states = null, setStates = () => {} }) => {
+  const [localFields, setLocalFields] = React.useState(fields);
+  useEffect(() => {
+    if (states) {
+      const newFields = fields.map((f) => f.map((row) => {
+        const { name } = row;
+        if (states[name]) {
+          return { ...row, value: states[name] };
+        }
+        return row;
+      }));
+      setLocalFields(newFields);
+    }
+  }, [states, fields]);
+  const handleChange = ({ name }) => (ev) => {
+    const { value } = ev.target;
+    const newFields = localFields.map((f) => f.map((r) => {
+      if (r.name === name) {
+        return { ...r, value };
+      }
+      return r;
+    }));
+    setLocalFields(newFields);
+    setStates((prev) => ({ ...prev, [name]: value }));
+  };
+  const getFieldView = ({ fieldType, accept, ...vProps }, idx) => {
     switch (fieldType) {
+      case 'file-field':
+        return <RRVFileUpload key={`fu-${idx}`} title={vProps.label} accept={accept} />;
       case 'text-field':
       default:
-        return <RRVInput key={`input-${idx}`} {...viewProps} />;
+        return <RRVInput key={`ft-${idx}`} onChange={handleChange(vProps)} {...vProps} />;
     }
   };
   const getSizes = (rowsLength) => {
@@ -17,9 +45,7 @@ export const RRVForm = ({ fields = [] }) => {
       lg: 12, md: 12, sm: 12, xs: 12,
     };
     if (rowsLength === 2) {
-      return {
-        ...sizes, lg: 6, md: 6, sm: 6,
-      };
+      return { ...sizes, lg: 6, md: 6 };
     }
     if (rowsLength === 3) {
       return { ...sizes, lg: 4, md: 4 };
@@ -32,17 +58,11 @@ export const RRVForm = ({ fields = [] }) => {
 
   return (
     <Grid container spacing={2}>
-      {fields.map((rows, fieldIdx) => {
-        const rowSizes = getSizes(rows.length);
-        console.log('RRVForm.getSizes', rows, rowSizes);
-        return (
-          <Grid item key={`field-grid-${fieldIdx}`} {...rowSizes}>
-            {rows.map((row, rowIdx) => (
-              getFieldView(row, rowIdx)
-            ))}
-          </Grid>
-        );
-      })}
+      {localFields.map((rows, fIdx) => rows.map((row, rIdx) => (
+        <Grid item key={`field-grid-${fIdx}-${rIdx}`} {...getSizes(rows.length)}>
+          {getFieldView(row, rIdx)}
+        </Grid>
+      )))}
     </Grid>
   );
 };

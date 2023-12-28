d125f846c8a0dfe60d14c95d673e2c7667626ac5
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-28 19:30:59 +0200

    Create media schema

---
+++ src/helpers/formSchemas/media.js
@@ -0,0 +1,43 @@
+export const mediaSchema = (albums = []) => [
+  [
+    {
+      label: 'Media title',
+      name: 'title',
+    },
+  ],
+  [
+    {
+      label: 'Media type',
+      name: 'type',
+      select: true,
+      options: [{ value: 'audio', label: 'Audio' }],
+    },
+    {
+      label: 'Media album',
+      name: 'albumId',
+      select: true,
+      options: albums,
+      valueSelector: 'id',
+      labelSelector: 'name',
+    },
+  ],
+  [
+    {
+      label: 'Author',
+      name: 'author',
+    },
+    {
+      label: 'Action date',
+      name: 'actionDate',
+      type: 'date',
+    },
+  ],
+  [
+    {
+      label: 'Media file',
+      fieldType: 'file-field',
+      type: 'audio',
+      accept: '.mp3, .m4a',
+    },
+  ],
+];

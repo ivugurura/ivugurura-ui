a1102ffefffc46641068519bc2cff072282da439
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-30 22:56:05 +0200

    Add comment columns

--- src/pages/admin/columns/commentariesColumns.js
+++ src/pages/admin/columns/commentariesColumns.js
@@ -1,11 +1,41 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useMemo } from 'react';
 
-export const commentariesColumns = () => [
-  { accessorKey: 'no', header: 'No' },
+import { Button, Tooltip, Typography } from '@mui/material';
+
+export const commentariesColumns = (setAction) => [
   { accessorKey: 'commentor', header: 'Commentor Info', size: 1 },
+  {
+    id: 'commentor',
+    header: 'Commentor Info',
+    Cell: ({ row }) => (
+      <Tooltip
+        title={(
+          <>
+            <Typography color="inherit">Tooltip with HTML</Typography>
+            <em>And here is</em>
+            {' '}
+            <b>some</b>
+            {' '}
+            <u>amazing content</u>
+            .
+            {' '}
+            It is very engaging. Right?
+          </>
+        )}
+      >
+        <Button>HTML</Button>
+      </Tooltip>
+    ),
+    size: 80,
+  },
   { accessorKey: 'content', header: 'Content' },
   { accessorKey: 'topicTitle', header: 'Topic Title' },
-  { accessorKey: 'isPublished', header: 'Is published' },
+  {
+    id: 'isPublished',
+    header: 'Published',
+    Cell: ({ row }) => <Button onClick={() => setAction(row.original)}>Publish</Button>,
+    size: 80,
+  },
   { accessorKey: 'actions', header: 'Actions' },
 ];

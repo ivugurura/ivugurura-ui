93c1aa8e691dd4ae8d1a77fb329f8de9ed3467cf
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-31 19:51:59 +0200

    Setup roboto css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import 'react-h5-audio-player/lib/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import 'suneditor/dist/css/suneditor.min.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');

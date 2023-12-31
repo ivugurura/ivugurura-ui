337e48a23118a9ad8d6e17e9cb1f224753855fec
Akimana A Jean d Amour <akimanaja17@gmail.com>
2023-12-31 20:27:36 +0200

    Remove unused

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,7 +1,6 @@
 export * from './HomeDashboard';
 export * from './TopicEditor';
 export * from './MediaEditor';
-export * from './Setting';
 export * from './SystemUsers';
 export * from './TopicEditor2';
 export * from './Commentary';

5a2c1197ee43116d86cbb85ede0287f2bd4a184d
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-01-04 18:54:48 +0200

    Apply static

--- src/common/components/navbar/Navbar.js
+++ src/common/components/navbar/Navbar.js
@@ -45,7 +45,7 @@ export const NavBar = () => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />

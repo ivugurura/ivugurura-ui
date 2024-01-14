56058349df282150e4e9be9b8aa694a92a6163f2
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-01-14 09:29:02 +0200

    Save comments

--- src/pages/TopicDetails/Comments.js
+++ src/pages/TopicDetails/Comments.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   Box, Button, Grid, Typography,
@@ -7,10 +7,20 @@ import {
 import { RRVForm } from '../../common/components/RRVForm';
 import { actions, initials } from '../../redux/apiSliceBuilder';
 
-import { commentSchema } from './schema';
+import { commentSchema, commentState } from './schema';
 
-export const Comments = () => {
-  const { data, isFetching } = actions.useGetCommentsTopicQuery();
+export const Comments = ({ slug }) => {
+  const [comment, setComment] = React.useState(commentState);
+  const [save, saveRes] = actions.useAddCommentTopicMutation();
+
+  const { data, isFetching } = actions.useViewCommentsTopicQuery({ slug });
+
+  useEffect(() => {
+    if (saveRes.isSuccess) {
+      setComment(commentState);
+      saveRes.reset();
+    }
+  }, [saveRes.isSuccess]);
   const { data: comments, totalItems } = data || initials.dataArr;
   console.log('Comments', { isFetching, comments, totalItems });
   return (
@@ -18,8 +28,13 @@ export const Comments = () => {
       <hr />
       <Box>Leave us a comment to this comment</Box>
       <Typography>We will not share your email</Typography>
-      <RRVForm fields={commentSchema()} />
-      <Button>Send</Button>
+      <RRVForm fields={commentSchema()} states={comment} setStates={setComment} />
+      <Button
+        onClick={() => save({ ...comment, slug })}
+        disabled={saveRes.isLoading}
+      >
+        {saveRes.isLoading ? 'Saving,...' : 'Send'}
+      </Button>
     </Grid>
   );
 };

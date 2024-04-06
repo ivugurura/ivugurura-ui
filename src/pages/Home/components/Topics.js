dd0a0732111d2c52f22afd68d510a12c29b1e419
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-04-07 01:06:05 +0200

    Add recent topics props

--- src/pages/Home/components/Topics.js
+++ src/pages/Home/components/Topics.js
@@ -5,18 +5,22 @@ import { Grid } from '@mui/material';
 import { actions, initials } from '../../../redux/apiSliceBuilder';
 import TopicItem from '../../TopicItem';
 
-export const HomeRecentTopics = () => {
-  const { data, isFetching } = actions.useGetRecentTopicsQuery({
-    truncate: 148,
+export const HomeRecentTopics = ({
+  xs = 12,
+  sm = 12,
+  md = 3,
+  truncate = 148,
+}) => {
+  const { data } = actions.useGetRecentTopicsQuery({
+    truncate,
   });
 
   const { data: topics } = data || initials.dataArr;
-  console.log({ topics, isFetching });
   return (
     <Grid container spacing={1}>
       {topics?.length > 0 &&
         topics.map((topic) => (
-          <Grid key={topic.title} item xs={12} sm={12} md={3}>
+          <Grid key={topic.title} item xs={xs} sm={sm} md={md}>
             <TopicItem topic={topic} hasMore />
           </Grid>
         ))}

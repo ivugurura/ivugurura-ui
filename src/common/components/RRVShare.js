79c4a9fe8f93b27558f6bf833be61b0f386229eb
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-04-07 14:34:14 +0200

    Use te reusable

--- src/common/components/RRVShare.js
+++ src/common/components/RRVShare.js
@@ -1,129 +1,43 @@
 import React from 'react';
 
 import {
-  KeyboardArrowDown as KeyboardArrowDownIcon,
   Facebook as FacebookIcon,
   WhatsApp as WhatsAppIcon,
   Twitter as TwitterIcon,
 } from '@mui/icons-material';
-import { Button, Menu, MenuItem } from '@mui/material';
-import { styled, alpha } from '@mui/material/styles';
 import {
   FacebookShareButton,
   TwitterShareButton,
   WhatsappShareButton,
 } from 'react-share';
 
-const StyledMenu = styled((props) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
-    }}
-    transformOrigin={{
-      vertical: 'top',
-      horizontal: 'right',
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  '& .MuiPaper-root': {
-    borderRadius: 6,
-    marginTop: theme.spacing(1),
-    minWidth: 180,
-    color:
-      theme.palette.mode === 'light'
-        ? 'rgb(55, 65, 81)'
-        : theme.palette.grey[300],
-    boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-    '& .MuiMenu-list': {
-      padding: '4px 0',
-    },
-    '& .MuiMenuItem-root': {
-      '& .MuiSvgIcon-root': {
-        fontSize: 18,
-        color: theme.palette.text.secondary,
-        marginRight: theme.spacing(1.5),
-      },
-      '&:active': {
-        backgroundColor: alpha(
-          theme.palette.primary.main,
-          theme.palette.action.selectedOpacity,
-        ),
-      },
-    },
-  },
-}));
+import { RRVDropdown } from './RRVDropdown';
 
 export const RRVShare = ({ onShare, href = '', title = '' }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return (
-    <>
-      <Button
-        id="share-button"
-        aria-controls={open ? 'share-munu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="contained"
-        disableElevation
-        size="small"
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
+  const shareOptions = [
+    {
+      wrapper: WhatsappShareButton,
+      startIcon: WhatsAppIcon,
+      title: 'WhatsApp',
+    },
+    {
+      wrapper: FacebookShareButton,
+      startIcon: FacebookIcon,
+      title: 'Facebook',
+    },
+    { wrapper: TwitterShareButton, startIcon: TwitterIcon, title: 'Twitter' },
+  ].map(
+    ({ wrapper: ShareWrapper, startIcon: ShareIcon, title: shareTitle }) => (
+      <ShareWrapper
+        hashtag={`#${title}`}
+        onClick={onShare}
+        quote={title}
+        url={href}
+        key={title}
       >
-        Share
-      </Button>
-      <StyledMenu
-        id="share-munu"
-        MenuListProps={{
-          'aria-labelledby': 'share-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-          <WhatsappShareButton
-            hashtag={`#${title}`}
-            onClick={onShare}
-            quote={title}
-            url={href}
-          >
-            <WhatsAppIcon />
-            WhatsApp
-          </WhatsappShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <FacebookShareButton
-            hashtag={`#${title}`}
-            onClick={onShare}
-            quote={title}
-            url={href}
-          >
-            <FacebookIcon />
-            Facebook
-          </FacebookShareButton>
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-          <TwitterShareButton
-            hashtag={`#${title}`}
-            onClick={onShare}
-            quote={title}
-            url={href}
-          >
-            <TwitterIcon />
-            Twitter
-          </TwitterShareButton>
-        </MenuItem>
-      </StyledMenu>
-    </>
+        <ShareIcon /> {shareTitle}
+      </ShareWrapper>
+    ),
   );
+  return <RRVDropdown title="Share" options={shareOptions} />;
 };

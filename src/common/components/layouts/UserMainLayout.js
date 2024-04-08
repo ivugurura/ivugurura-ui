63294d6a032420a176c2c6c76b2332a0c8a8855a
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-04-08 21:57:45 +0200

    Fetch nav categories

--- src/common/components/layouts/UserMainLayout.js
+++ src/common/components/layouts/UserMainLayout.js
@@ -3,21 +3,24 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
+import { actions } from '../../../redux/actions';
+import { initials } from '../../../redux/apiSliceBuilder';
 import { useStyles } from '../../styles';
-// import { Footer } from '../Footer';
 import { MainFooter } from '../Footer';
 import { NavBar } from '../navbar';
 
 export const UserMainLayout = () => {
   const classes = useStyles;
+  const { data: catData } = actions.useGetNavsConfigQuery();
+  const { data: categories } = catData || initials.dataArr;
   return (
     <Box>
-      <NavBar />
+      <NavBar navCategories={categories} />
       <main className={classes.content}>
         <Outlet />
       </main>
       <div style={{ marginTop: 10 }} />
-      <MainFooter />
+      <MainFooter navCategories={categories} />
     </Box>
   );
 };

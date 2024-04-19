faf50b2acf15eb04bb64333460a24bae0b745acd
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-04-19 23:16:26 +0200

    Update language

--- src/i18n.js
+++ src/i18n.js
@@ -17,8 +17,9 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: defaultLng,
     debug: true,
+    lng: defaultLng,
+    fallbackLng: 'en',
   });
 
 export default i18n;

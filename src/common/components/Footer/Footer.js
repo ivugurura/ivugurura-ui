5475d75b3ca4b3500560a7f116ef8a7e880c19e9
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-05-05 08:28:28 +0200

    Refactor lang on footer

--- src/common/components/Footer/Footer.js
+++ src/common/components/Footer/Footer.js
@@ -6,109 +6,13 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
-import { socialMedias, systemLanguage } from '../../../helpers/utils/constants';
+import { socialMedias } from '../../../helpers/utils/constants';
 import { Copyright } from '../Copyright';
+import { useLang } from '../providers';
 
-export const navCategoriesd = [
-  {
-    id: 13,
-    name: 'Amagara mazima',
-    slug: 'amagara-mazima-cqzkfgehpjo',
-    languageId: 1,
-    createdAt: '2020-09-24T05:53:36.660Z',
-    updatedAt: '2020-09-24T05:53:36.660Z',
-    categoryId: null,
-    categories: [
-      {
-        id: 14,
-        name: 'Ubuzima',
-        slug: 'ubuzima-m75kfiqfemj',
-      },
-    ],
-  },
-  {
-    id: 9,
-    name: 'Ubuhanuzi',
-    slug: 'ubuhanuzi-cqzkfgeg2wo',
-    languageId: 1,
-    createdAt: '2020-09-24T05:52:20.664Z',
-    updatedAt: '2020-09-24T05:52:20.664Z',
-    categoryId: null,
-    categories: [
-      {
-        id: 10,
-        name: "Ubuhanuzi bw'ahashize",
-        slug: 'ubuhanuzi-bw-ahashize-cqzkfgegito',
-      },
-      {
-        id: 12,
-        name: "Ubuhanuzi bw'ahazaza",
-        slug: 'ubuhanuzi-bw-ahazaza-cqzkfgehd9d',
-      },
-      {
-        id: 11,
-        name: "Ubuhanuzi bw'iki gihe",
-        slug: 'ubuhanuzi-bw-iki-gihe-cqzkfgegy3j',
-      },
-    ],
-  },
-  {
-    id: 6,
-    name: 'Imibereho myiza',
-    slug: 'imibereho-myiza-cqzkfgeejzv',
-    languageId: 1,
-    createdAt: '2020-09-24T05:51:09.499Z',
-    updatedAt: '2020-09-24T05:51:09.499Z',
-    categoryId: null,
-    categories: [
-      {
-        id: 8,
-        name: 'Umuryango',
-        slug: 'umuryango-cqzkfgefdl7',
-      },
-      {
-        id: 7,
-        name: 'Uburezi',
-        slug: 'uburezi-cqzkfgeeygd',
-      },
-    ],
-  },
-  {
-    id: 1,
-    name: 'Ubukristo',
-    slug: 'ubukristo-cqzkfgec1lk',
-    languageId: 1,
-    createdAt: '2020-09-24T05:49:12.344Z',
-    updatedAt: '2020-09-24T05:49:12.344Z',
-    categoryId: null,
-    categories: [
-      {
-        id: 5,
-        name: "Itorero ry'Imana",
-        slug: 'itorero-ry-imana-cqzkfgedunh',
-      },
-      {
-        id: 3,
-        name: 'Gutsindishirizwa',
-        slug: 'gutsindishirizwa-cqzkfged033',
-      },
-      {
-        id: 4,
-        name: "Ukuri kw'iki gihe",
-        slug: 'ukuri-kw-iki-gihe-cqzkfgedmo9',
-      },
-      {
-        id: 2,
-        name: 'Babuloni',
-        slug: 'babuloni-cqzkfgecooy',
-      },
-    ],
-  },
-];
 export const MainFooter = ({ navCategories = [] }) => {
   const { t } = useTranslation();
-  // const { navCategories } = useSelector(({ category }) => category);
-  console.log('Footer');
+  const { lang } = useLang();
   return (
     <footer className="mainfooter" role="contentinfo">
       <div className="footer-middle">
@@ -124,7 +28,7 @@ export const MainFooter = ({ navCategories = [] }) => {
                         {category.categories.map((subCat) => (
                           <Link
                             key={subCat.slug}
-                            to={`/${systemLanguage}/topics/categories/${subCat.slug}`}
+                            to={`/${lang}/topics?t=${subCat.slug}`}
                           >
                             <p>{subCat.name}</p>
                           </Link>
@@ -153,6 +57,7 @@ export const MainFooter = ({ navCategories = [] }) => {
               </ul>
               <address>
                 {t('contactUs')}:
+                <br />
                 <a
                   href="mailto:abagorozi@yahoo.com"
                   rel="noreferrer"

99ab747e8ed38c44fc7b72d9a97f58c9cdfb8be1
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-06-22 21:02:13 +0200

    Remove styles

--- src/common/components/layouts/UserMainLayout.js
+++ src/common/components/layouts/UserMainLayout.js
@@ -5,18 +5,16 @@ import { Outlet } from 'react-router-dom';
 
 import { actions } from '../../../redux/actions';
 import { initials } from '../../../redux/apiSliceBuilder';
-import { useStyles } from '../../styles';
 import { MainFooter } from '../Footer';
 import { NavBar } from '../navbar';
 
 export const UserMainLayout = () => {
-  const classes = useStyles;
   const { data: catData } = actions.useGetNavsConfigQuery();
   const { data: categories } = catData || initials.dataArr;
   return (
     <Box>
       <NavBar navCategories={categories} />
-      <main className={classes.content}>
+      <main>
         <Outlet />
       </main>
       <div style={{ marginTop: 10 }} />

bd10568f47a4c01ffbd9752f26e8f428218ff848
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-07-13 21:07:38 +0200

    Chech auth user from backend

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -55,6 +55,34 @@ export const uploadFileWithProgress = (
   });
 };
 
+export const validateUserAuthentication = async () => {
+  const result = {
+    isAuthenticated: false,
+    errorMessage: '',
+    data: null,
+  };
+
+  return http
+    .get('/users/my-profile')
+    .then((response) => {
+      if (response.data.data) {
+        result.isAuthenticated = true;
+        result.data = response.data.data;
+      } else {
+        result.errorMessage = response.data.message;
+      }
+      return result;
+    })
+    .catch((error) => {
+      if (error.response) {
+        result.errorMessage = error.response.data.message;
+      } else {
+        result.errorMessage = error.message;
+      }
+      return result;
+    });
+};
+
 export const startCase = (str = '', toUpper = true) => {
   const sentences = str.toLowerCase().split(' ');
   sentences.forEach((_, i) => {

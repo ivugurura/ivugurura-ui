7401a2d546c43f2ef16ea6c1320dc8615e311f97
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-07-13 21:03:40 +0200

    Clean up http

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
 
-import { systemLanguage } from './utils/constants';
-
-let token = 'null';
-if (localStorage.user) {
-  const user = JSON.parse(localStorage.user);
-  token = user.token;
-}
+import { systemLanguage, lStorage } from './utils/constants';
 
 export const VERBS = {
   get: 'GET',
@@ -20,27 +14,7 @@ export const http = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api/v1`,
   withCredentials: true,
   headers: {
-    Authorization: token,
+    Authorization: lStorage.token,
     'Accept-Language': systemLanguage,
   },
 });
-
-export const uploadFileWithProgress = (
-  file,
-  type = '',
-  prevFile = '',
-  onUploadProgress = () => {},
-) => {
-  const formData = new FormData();
-
-  formData.append('file', file);
-
-  const uploadUrl = `/albums/upload/${type}?prevFile=${prevFile}`;
-
-  return http.post(uploadUrl, formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-    onUploadProgress,
-  });
-};

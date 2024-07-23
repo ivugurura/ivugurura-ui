829522c668a2345fcf062fdf8010c03cc833356f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-07-23 09:58:33 +0200

    Display more audios

--- src/pages/AudiosPage/index.js
+++ src/pages/AudiosPage/index.js
@@ -11,7 +11,7 @@ const AudiosPage = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={7}>
-        <RRVAudioPlayer displayText={width > 425} />
+        <RRVAudioPlayer displayText={width > 425} nOfAudios={20} />
       </Grid>
       <Grid item xs={12} md={5}>
         <HomeRecentTopics xs={12} sm={12} md={12} truncate={164} />

eedfc0328aca05bb3f4a841d44fb97766f90add7
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-07-23 09:34:31 +0200

    Fetch youtube data

--- src/pages/Home/components/HomeYoutube.js
+++ src/pages/Home/components/HomeYoutube.js
@@ -3,35 +3,33 @@ import React, { useEffect } from 'react';
 import YouTube from 'react-youtube';
 
 import { useWindowSize } from '../../../common/hooks/useWindowSize';
-// import { actions } from '../../../redux/actions';
+import { actions } from '../../../redux/actions';
+import { initials } from '../../../redux/apiSliceBuilder';
 
 import { YoutubeLoader } from './HomeLoaders';
 
 export const HomeYoutube = ({
   widthRatio = 0.485,
   heightRatio = 0.7,
-  normalHeight = 370,
+  normalHeight = 360,
   randomize = false,
 }) => {
   const [videoId, setVideoId] = React.useState('-PweyZWNcLk');
   const [playerSizes, setPlayerSizes] = React.useState({});
   const { width: winWidth, height: winHeight } = useWindowSize();
-  // const { data: ytData, isFetching } = actions.useListYoutubesQuery();
-  const { data: ytData, isFetching } = {
-    data: { items: [], isFetching: false },
-  };
+  const { data, isFetching } = actions.useListYoutubesQuery();
+  const { data: ytData } = data || initials.dataObj;
   useEffect(() => {
     let width = winWidth * widthRatio;
-    let height = winHeight * heightRatio;
+    const height = normalHeight;
     if (winWidth < 900) {
       width = winWidth - 30;
-      height = normalHeight;
     }
     setPlayerSizes({ height, width });
   }, [widthRatio, heightRatio, winHeight, winWidth, normalHeight]);
 
   useEffect(() => {
-    if (ytData?.items.length > 0) {
+    if (ytData?.items?.length > 0) {
       if (randomize) {
         const randomIndex = Math.floor(Math.random() * ytData.items.length);
         setVideoId(ytData.items[randomIndex].id.videoId);

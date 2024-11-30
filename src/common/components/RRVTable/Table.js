a8e7b79d9299e2ccf51ce27978d63d0b605e9791
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-11-30 22:45:18 +0200

    Add table selection support

--- src/common/components/RRVTable/Table.js
+++ src/common/components/RRVTable/Table.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
+import { Button } from '@mui/material';
 import { MaterialReactTable } from 'material-react-table';
 
 export const RRVTable = ({
   isLoading,
   globalFilter,
   setGlobalFilter,
+  rowSelection,
+  setRowSelection,
+  onHandleSelected = () => {},
+  btnSelectionLabel,
   ...rest
 }) => (
   <MaterialReactTable
-    state={{ isLoading, globalFilter }}
+    state={{ isLoading, globalFilter, rowSelection }}
     onGlobalFilterChange={setGlobalFilter}
+    onRowSelectionChange={setRowSelection}
     manualFiltering
     positionActionsColumn="last"
+    renderTopToolbarCustomActions={
+      btnSelectionLabel
+        ? ({ table: { getSelectedRowModel } }) => {
+            const { rows } = getSelectedRowModel();
+            return (
+              <Button
+                onClick={() => {
+                  onHandleSelected(rows);
+                }}
+              >
+                {`${btnSelectionLabel} (${rows.length})`}
+              </Button>
+            );
+          }
+        : undefined
+    }
     {...rest}
   />
 );

2f0143d6d87d93e82a1b5459a1fd4efcd84b4950
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-05 15:35:22 +0200

    Add pagination and fix null

--- src/common/components/RRVTable/Table.js
+++ src/common/components/RRVTable/Table.js
@@ -7,22 +7,27 @@ export const RRVTable = ({
   isLoading,
   globalFilter,
   setGlobalFilter,
-  rowSelection,
+  rowSelection = {},
+  pagination = { pageIndex: 0, pageSize: 10 },
+  setPagination,
   setRowSelection,
   onHandleSelected = () => {},
   btnSelectionLabel,
   ...rest
 }) => (
   <MaterialReactTable
-    state={{ isLoading, globalFilter, rowSelection }}
+    state={{ isLoading, globalFilter, rowSelection, pagination }}
     onGlobalFilterChange={setGlobalFilter}
     onRowSelectionChange={setRowSelection}
+    onPaginationChange={setPagination}
     manualFiltering
+    manualPagination
     positionActionsColumn="last"
     renderTopToolbarCustomActions={
       btnSelectionLabel
         ? ({ table: { getSelectedRowModel } }) => {
             const { rows } = getSelectedRowModel();
+            if (rows.length === 0) return null;
             return (
               <Button
                 onClick={() => {

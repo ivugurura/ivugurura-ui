7193155ba05344afeb16bb84338d40c1893a6a50
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-05 19:50:58 +0200

    Build alert hook

---
+++ src/common/hooks/useAlertDialog.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const alertInitial = {
+  current: null,
+  actionType: '',
+  message: '',
+  open: false,
+};
+export const useAlertDialog = () => {
+  const [alertData, setAlertData] = useState(alertInitial);
+
+  const reset = () => {
+    setAlertData(alertInitial);
+  };
+
+  const setAlertValues = (values = {}) => {
+    const newValues = Object.entries(values).map(([key, value]) => ({
+      [key]: value,
+    }));
+    setAlertData((prev) => ({ ...prev, ...newValues }));
+  };
+
+  return { alertValues: alertData, reset, setAlertValues };
+};

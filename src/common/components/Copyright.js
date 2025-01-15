ea16e803509a85a8512a8cba81548022c787629f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-01-15 09:22:29 +0200

    Link privacy policy

--- src/common/components/Copyright.js
+++ src/common/components/Copyright.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 import { currentYear } from '../../helpers/utils/constants';
 
@@ -8,5 +9,8 @@ export const Copyright = (props) => (
   <Typography variant="body2" color="text.secondary" align="center" {...props}>
     &copy;
     {` Copyright 2016-${currentYear}, Ivugurura n Ubugorozi. All rights reserved.`}
+    <Link to="/privacy-policy">
+      <p>Privacy Policy</p>
+    </Link>
   </Typography>
 );

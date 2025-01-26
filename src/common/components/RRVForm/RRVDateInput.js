6d8d3b41adcefa4017e3c2f9cdf9c570a5d019b6
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-01-26 13:02:01 +0200

    Create reusable date input

---
+++ src/common/components/RRVForm/RRVDateInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
+export const RRVDateInput = (props) => {
+  const { value, ...otherProps } = props;
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker value={value || null} {...otherProps} />
+    </LocalizationProvider>
+  );
+};

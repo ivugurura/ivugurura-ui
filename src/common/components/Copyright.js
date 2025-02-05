a8f2cf133d0e8271aec682288cb60a657b5cfb52
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-02-05 19:03:41 +0200

    Fix warning

--- src/common/components/Copyright.js
+++ src/common/components/Copyright.js
@@ -9,8 +9,8 @@ export const Copyright = (props) => (
   <Typography variant="body2" color="text.secondary" align="center" {...props}>
     &copy;
     {` Copyright 2016-${currentYear}, Ivugurura n Ubugorozi. All rights reserved.`}
-    <Link to="/privacy-policy">
-      <p>Privacy Policy</p>
+    <Link to="/privacy-policy" style={{ display: 'block' }}>
+      <span>Privacy Policy</span>
     </Link>
   </Typography>
 );

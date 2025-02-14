f2541106f873ec6f3b3582b926ad9614cdf1dce5
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-02-14 13:43:33 +0200

    Fetch books

--- src/pages/admin/Books/index.js
+++ src/pages/admin/Books/index.js
@@ -3,34 +3,40 @@ import React, { useState } from 'react';
 import { Button } from '@mui/material';
 
 import { BooksList } from '../../../common/components/BooksList';
+import { useMuiSearchPagination } from '../../../common/hooks/useMuiSearchPagination';
+import { actions, initials } from '../../../redux/apiSliceBuilder';
 import { DashboardContainer } from '../components/DashboardContainer';
 
 import { AddEditBook } from './AddEditBook';
 
-const initialBooks = [
-  {
-    id: 1,
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
-    description:
-      'A classic novel about racial injustice in the American South.',
-    coverImage: '/api/placeholder/200/300',
-    genre: 'Fiction',
-    publishedYear: 1960,
-  },
-  {
-    id: 2,
-    title: '1984',
-    author: 'George Orwell',
-    description:
-      'A dystopian novel exploring totalitarianism and government surveillance.',
-    coverImage: '/api/placeholder/200/300',
-    genre: 'Dystopian Fiction',
-    publishedYear: 1949,
-  },
-];
+// const initialBooks = [
+//   {
+//     id: 1,
+//     title: 'To Kill a Mockingbird',
+//     author: 'Harper Lee',
+//     description:
+//       'A classic novel about racial injustice in the American South.',
+//     coverImage: '/api/placeholder/200/300',
+//     genre: 'Fiction',
+//     publishedYear: 1960,
+//   },
+//   {
+//     id: 2,
+//     title: '1984',
+//     author: 'George Orwell',
+//     description:
+//       'A dystopian novel exploring totalitarianism and government surveillance.',
+//     coverImage: '/api/placeholder/200/300',
+//     genre: 'Dystopian Fiction',
+//     publishedYear: 1949,
+//   },
+// ];
 const Books = () => {
   const [openBookModal, setOpenBookModal] = useState(false);
+  const { paginator } = useMuiSearchPagination();
+  const { data, refetch } = actions.useListBooksQuery(paginator);
+
+  const { data: books } = data || initials.dataArr;
 
   return (
     <DashboardContainer
@@ -39,10 +45,11 @@ const Books = () => {
         <Button onClick={() => setOpenBookModal(true)}>Add new book</Button>
       }
     >
-      <BooksList books={initialBooks} />
+      <BooksList books={books} />
       <AddEditBook
         open={openBookModal}
         onClose={() => setOpenBookModal(false)}
+        refetchBooks={refetch}
       />
     </DashboardContainer>
   );

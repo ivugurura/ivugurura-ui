901fc81861e415c7cdef4516f69eea8ecde1b83f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-08 23:12:48 +0200

    Remove edit and clean container

--- src/common/components/BooksList.js
+++ src/common/components/BooksList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {
   Delete as DeleteIcon,
-  EditNoteOutlined as EditIcon,
+  // EditNoteOutlined as EditIcon,
   ViewAgendaOutlined as ViewIcon,
 } from '@mui/icons-material';
 import {
@@ -11,7 +11,6 @@ import {
   CardActions,
   CardContent,
   CardMedia,
-  Container,
   Grid,
   Typography,
 } from '@mui/material';
@@ -23,57 +22,57 @@ export const BooksList = ({
   onBookClick = () => {},
   isAdmin = false,
 }) => (
-  <Container sx={{ py: 4 }}>
-    <Grid container spacing={3}>
-      {books.map((book) => (
-        <Grid item xs={12} sm={6} md={4} key={book.id}>
-          <Card
-            sx={{
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              cursor: 'pointer',
-            }}
-            onClick={() => onBookClick(book)}
-          >
-            <CardMedia
-              component="img"
-              height="300"
-              image={`${BOOK_COVERS_PATH}/${book.coverImage}`}
-              alt={book.name}
-            />
-            <CardContent sx={{ flexGrow: 1 }}>
-              <Typography gutterBottom variant="h5" component="div">
-                {book.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                by Ivugurura n Ubugorozi
-              </Typography>
-            </CardContent>
-            {isAdmin && (
-              <CardActions>
-                <Button
-                  size="small"
-                  startIcon={<ViewIcon />}
-                  onClick={() => onBookClick(book, 'read')}
-                >
-                  Read
-                </Button>
-                <Button size="small" startIcon={<EditIcon />}>
+  <Grid container spacing={3}>
+    {books.map((book) => (
+      <Grid item xs={12} sm={6} md={4} key={book.id}>
+        <Card
+          sx={{
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            cursor: 'pointer',
+          }}
+          onClick={() => onBookClick(book)}
+        >
+          <CardMedia
+            component="img"
+            height="300"
+            image={`${BOOK_COVERS_PATH}/${book.coverImage}`}
+            alt={book.name}
+          />
+          <CardContent sx={{ flexGrow: 1 }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {book.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              by Ivugurura n Ubugorozi
+            </Typography>
+          </CardContent>
+          {isAdmin && (
+            <CardActions>
+              <Button
+                size="small"
+                color="primary"
+                startIcon={<ViewIcon />}
+                onClick={() => onBookClick(book, 'read')}
+              >
+                Read
+              </Button>
+              {/* <Button size="small" startIcon={<EditIcon />}>
                   Edit
-                </Button>
-                <Button
-                  size="small"
-                  startIcon={<DeleteIcon />}
-                  onClick={() => onBookClick(book, 'delete')}
-                >
-                  Delete
-                </Button>
-              </CardActions>
-            )}
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
-  </Container>
+                </Button> */}
+              <Button
+                size="small"
+                color="secondary"
+                startIcon={<DeleteIcon />}
+                onClick={() => onBookClick(book, 'delete')}
+              >
+                Delete
+              </Button>
+            </CardActions>
+          )}
+        </Card>
+      </Grid>
+    ))}
+  </Grid>
 );

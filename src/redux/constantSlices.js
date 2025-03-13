5e74217a5ce539a2b53b74cbe1b4ff8a8ac19201
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-13 20:49:07 +0200

    Direct file download

--- src/redux/constantSlices.js
+++ src/redux/constantSlices.js
@@ -41,3 +41,31 @@ export const authUserSlice = createSlice({
     },
   },
 });
+
+// export const pdfsSlice = apiSlice.injectEndpoints({
+//   baseQuery: fetchBaseQuery({ baseUrl: '/ ' }),
+//   endpoints: (builder) => ({
+//     downloadPDFFile: builder.mutation({
+//       queryFn: async ({ setupId, name }, api, extraOptions, baseQuery) => {
+//         const result = await baseQuery({
+//           url: `/setups/${setupId}/file`,
+//           responseHandler: (response) => response.blob(),
+//         });
+//         var hiddenElement = document.createElement('a');
+//         var url = window.URL || window.webkitURL;
+//         var blobPDF = url.createObjectURL(result.data);
+//         hiddenElement.href = blobPDF;
+//         hiddenElement.target = '_blank';
+//         hiddenElement.download = `${name}_report.pdf`;
+//         hiddenElement.click();
+//         setTimeout(() => {
+//           document.body.removeChild(hiddenElement);
+//           url.revokeObjectURL(blobPDF);
+//         }, 2000);
+//         return { data: null };
+//       },
+//     }),
+//   }),
+// });
+
+// export const { useDownloadPDFFileMutation } = pdfsSlice;

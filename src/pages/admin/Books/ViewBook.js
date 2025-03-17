428899c6a4071766e363c81f7e9987ddb7925539
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-17 12:57:35 +0200

    Cleanup the old implementation

--- src/pages/admin/Books/ViewBook.js
+++ src/pages/admin/Books/ViewBook.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Dialog } from '@mui/material';
 
 import { PdfViewerV3 } from '../../../common/components/PDFViewerV3';
-import { actions } from '../../../redux/apiSliceBuilder';
 
 export const ViewBook = ({
   book = {},
@@ -11,22 +10,7 @@ export const ViewBook = ({
   onClose = () => {},
   fullScreen = false,
 }) => {
-  const [downloadBook, res] = actions.useDownloadBookMutation();
   console.log(`${process.env.REACT_APP_API_URL}/api/v1/books/${book.id}`);
-
-  React.useEffect(() => {
-    if (book.id && res.isSuccess) {
-      const url = window.URL.createObjectURL(res.data);
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${book.name}.pdf`);
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-    }
-  }, [res.isSuccess, book.id]);
-  if (!book.id) return null;
-  console.log(res);
 
   return (
     <Dialog

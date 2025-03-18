3c3b4e04996de14ac45a547564849c1c13e0ace9
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-18 10:20:49 +0200

    Display errors

--- src/common/components/RRVDownloadBtn.js
+++ src/common/components/RRVDownloadBtn.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 
 import { Button } from '@mui/material';
+import { toast } from 'react-toastify';
 
 import { actions } from '../../redux/apiSliceBuilder';
 
 export const RRVDownloadBtn = ({ useMutation, params = {} }) => {
-  const [downladFile, { data, isLoading, isSuccess }] = actions[useMutation]();
+  const [downladFile, { data, isLoading, isSuccess, error }] =
+    actions[useMutation]();
 
   useEffect(() => {
     if (isSuccess && data?.blob) {
@@ -19,6 +21,15 @@ export const RRVDownloadBtn = ({ useMutation, params = {} }) => {
       window.URL.revokeObjectURL(url); // Cleanup
     }
   }, [data]);
+
+  useEffect(() => {
+    if (error?.error) {
+      toast.error(error.error, {
+        position: 'bottom-right',
+        toastId: 13,
+      });
+    }
+  }, [error?.error]);
 
   return (
     <Button onClick={() => downladFile(params)} disabled={isLoading}>

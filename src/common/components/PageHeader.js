3a793f5b20eb8d38b2267ff923bbb9e8d27cb7a3
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-04-01 17:55:05 +0200

    Make title optional

--- src/common/components/PageHeader.js
+++ src/common/components/PageHeader.js
@@ -5,9 +5,11 @@ import { Box, Typography } from '@mui/material';
 export const PageHeader = ({ title, description }) => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" py={2}>
-      <Typography variant="subtitle2" py={4}>
-        {title}
-      </Typography>
+      {title && (
+        <Typography variant="subtitle2" py={4}>
+          {title}
+        </Typography>
+      )}
       <Typography variant="h3" fontWeight={800}>
         {description}
       </Typography>

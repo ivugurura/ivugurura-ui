b0f6aa60c1cc0b54f5f2fd02fdce4e26079044f6
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-04-05 22:03:59 +0200

    Handle navigations

--- src/common/components/BooksList.js
+++ src/common/components/BooksList.js
@@ -14,8 +14,9 @@ import {
   Grid,
   Typography,
 } from '@mui/material';
+import { useNavigate } from 'react-router';
 
-import { BOOK_COVERS_PATH } from '../../helpers/utils/constants';
+import { BOOK_COVERS_PATH, toLink } from '../../helpers/utils/constants';
 
 const getCoverImage = (img) => {
   let cover = `${BOOK_COVERS_PATH}/${img}`;
@@ -28,58 +29,65 @@ export const BooksList = ({
   books = [],
   onBookClick = () => {},
   isAdmin = false,
-}) => (
-  <Grid container spacing={3}>
-    {books.map((book) => (
-      <Grid item xs={12} sm={6} md={4} key={book.id}>
-        <Card
-          sx={{
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            cursor: 'pointer',
-          }}
-          onClick={() => onBookClick(book)}
-        >
-          <CardMedia
-            component="img"
-            height="300"
-            image={getCoverImage(book.coverImage)}
-            alt={book.name}
-          />
-          <CardContent sx={{ flexGrow: 1 }}>
-            <Typography gutterBottom variant="h5" component="div">
-              {book.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              by Ivugurura n Ubugorozi
-            </Typography>
-          </CardContent>
-          {isAdmin && (
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                startIcon={<ViewIcon />}
-                onClick={() => onBookClick(book, 'read')}
-              >
-                Read
-              </Button>
-              {/* <Button size="small" startIcon={<EditIcon />}>
+}) => {
+  const navigate = useNavigate();
+
+  const handleReadBook = (book) => {
+    navigate(toLink(`/library/${book.slug}`, isAdmin));
+  };
+  return (
+    <Grid container spacing={3}>
+      {books.map((book) => (
+        <Grid item xs={12} sm={6} md={4} key={book.id}>
+          <Card
+            sx={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              cursor: 'pointer',
+            }}
+            onClick={() => handleReadBook(book)}
+          >
+            <CardMedia
+              component="img"
+              height="300"
+              image={getCoverImage(book.coverImage)}
+              alt={book.name}
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="div">
+                {book.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                by Ivugurura n Ubugorozi
+              </Typography>
+            </CardContent>
+            {isAdmin && (
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  startIcon={<ViewIcon />}
+                  onClick={() => handleReadBook(book)}
+                >
+                  Read
+                </Button>
+                {/* <Button size="small" startIcon={<EditIcon />}>
                   Edit
                 </Button> */}
-              <Button
-                size="small"
-                color="secondary"
-                startIcon={<DeleteIcon />}
-                onClick={() => onBookClick(book, 'delete')}
-              >
-                Delete
-              </Button>
-            </CardActions>
-          )}
-        </Card>
-      </Grid>
-    ))}
-  </Grid>
-);
+                <Button
+                  size="small"
+                  color="secondary"
+                  startIcon={<DeleteIcon />}
+                  onClick={() => onBookClick(book, 'delete')}
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            )}
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};

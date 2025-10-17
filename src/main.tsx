b043c607776c2426172339a810f48810a1ed065e
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-17 16:34:28 +0200

    Test the configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
@@ -13,5 +13,8 @@ import '@fontsource/roboto/700.css';
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-createRoot(rootElement).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);

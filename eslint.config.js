41d2c09382092858a3f33d6232b6aed9e4f3075c
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-19 10:05:13 +0200

    Refactor eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,32 +5,30 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['build'] },
-  {
-    extends: [
-      js.configs.recommended,
-      importPlugin.flatConfigs.recommended,
-      importPlugin.flatConfigs.typescript,
-      ...tseslint.configs.recommended,
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-        // Add a rule to ignore _ char
-        { ignorePattern: '^_' },
-      ],
-    },
+export default tseslint.config({
+  ignores: ['build'],
+  extends: [
+    js.configs.recommended,
+    importPlugin.flatConfigs.recommended,
+    importPlugin.flatConfigs.typescript,
+    ...tseslint.configs.recommended,
+  ],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-);
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+      // Add a rule to ignore _ char
+      { ignorePattern: '^_' },
+    ],
+  },
+});

c2de822260367c84ec4b2208ae1b06b00fbb2cbb
Akimana A Jean d Amour <akimanaja17@gmail.com>
2026-02-25 17:34:39 +0200

    Fix a typo

--- src/common/components/RRVEditor/SunEditor.tsx
+++ src/common/components/RRVEditor/SunEditor.tsx
@@ -1,4 +1,4 @@
-import type React from 'react';
+import React from 'react';
 
 import SunEditor from 'suneditor-react';
 import type { SunEditorReactProps } from 'suneditor-react/dist/types/SunEditorReactProps';
--- src/common/components/wrappers/PageHelmet.tsx
+++ src/common/components/wrappers/PageHelmet.tsx
@@ -1,4 +1,4 @@
-import type React from 'react';
+import React from 'react';
 
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
